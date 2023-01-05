ffcf4014f5c2fd37a7b005bb1bba63fce4e26682
M Dederi <mjdederi01@gmail.com>
2023-01-05 11:41:11 -0800

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import sys
+if __name__ == '__main__':
+    import sys
 
-ans = 0
-n = len(sys.argv)
+    arg_sum = 0
+    arg_len = len(sys.argv)
 
-for i in range(1, n):
-    ans = ans + int(sys.argv[i])
-print('{:d}'.format(ans))
+    for i in range(1, arg_len):
+        arg_sum += int(sys.argv[i])
+    print('{:d}'.format(arg_sum))
