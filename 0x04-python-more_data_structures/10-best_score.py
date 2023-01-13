9c0882497035e34933b4cb9cf2c6235b29fd09dc
M Dederi <mjdederi01@gmail.com>
2023-01-13 06:24:33 -0800

    dict op

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        for key in enumerate(sorted(a_dictionary.keys())):
-            return key[-1]
+        li = []
+        for elem in sorted(a_dictionary.items(), key=lambda x: x[1]):
+            li.append(elem[0])
+        return li[-1]
