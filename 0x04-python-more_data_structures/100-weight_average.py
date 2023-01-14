285ee3f56ef2033e77ce5bc587c7471b91dbbbc7
M Dederi <mjdederi01@gmail.com>
2023-01-14 08:00:47 -0800

    Advanced

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if my_list:
+        sum01 = sum1 = 0
+        for elem in my_list:
+            sum01 += (elem[0] * elem[1])
+            sum1 += elem[1]
+        return (sum01 / sum1)
