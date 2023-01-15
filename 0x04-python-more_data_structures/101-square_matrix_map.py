f5fcc0d03d7e85330803a8067b17bd6fd31afadb
M Dederi <mjdederi01@gmail.com>
2023-01-15 04:27:07 -0800

    matrix op

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda mem: list(map(lambda elem: elem * elem, mem)), matrix))
+    return list(map(lambda mem: list(map(lambda el: el * el, mem)), matrix))
